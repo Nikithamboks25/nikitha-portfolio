b061801a365e326ff652fef59fd98cd8208b167f
Nikithamboks25 <Nikitha.mbokotwana@capaciti.org.za>
2024-11-21 15:56:31 +0200

    added picture on achievements

--- src/pages/Achievements.js
+++ src/pages/Achievements.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './../styles/Project.css'; 
 import FullstackPicture from "../assets/fullstack.jpg";
 import DiplomaPicture from "../assets/diploma.jpg";
+import HigherCertificate from "../assets/HC.jpg"
 
 const Achievements = () => {
   return (
@@ -19,6 +20,11 @@ const Achievements = () => {
         <div className="project-card">
           <h3>Capaciti Fullstack Certification</h3>
           <img src={FullstackPicture} alt="Fullstack Certification" className="project-image" style={{ width: "350px", height: "auto"}}/>
+        </div>
+        {/* Achievement 3 */}
+        <div className="project-card">
+          <h3>Cape Peninsula University of Technology Certificate</h3>
+          <img src={HigherCertificate} alt=" Higher Certification" className="project-image" style={{ width: "350px", height: "auto"}}/>
         </div>
       </div>
     </section>
