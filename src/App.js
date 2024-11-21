583cd6fb09e45c8dd8c7b0af969b2f32d0170c31
Nikithamboks25 <Nikitha.mbokotwana@capaciti.org.za>
2024-11-21 14:06:38 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -2,23 +2,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import Projects from './pages/Project';
-import Skills from './pages/Skills';
 import Achievements from './pages/Achievements';
 import ContactUs from './pages/ContactUs';
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/achievements" element={<Achievements />} />
-        <Route path="/contact" element={<ContactUs />} />
-      </Routes>
+      <div className="App">
+        <Navbar />
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/achievements" element={<Achievements />} />
+            <Route path="/contact" element={<ContactUs />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
     </Router>
   );
 }
