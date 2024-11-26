a573c67b8f785185ddc44666894a48c31512c4ea
Nikithamboks25 <you@example.com>
2024-11-26 09:05:42 +0100

    fixing alignment of navbar and implementing modal

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,31 @@
-// src/Navbar.js
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css'; // Import the CSS file for styling
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const location = useLocation();
+
+  // Close the menu on route change
+  useEffect(() => {
+    setIsMenuOpen(false);
+  }, [location]);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav>
       <div className="logo">
-      <h2><em>NM.</em></h2>
+        <h2><em>NM.</em></h2>
       </div>
-      <ul>
+      <div className="burger" onClick={toggleMenu}>
+        <div className={`line ${isMenuOpen ? 'open' : ''}`}></div>
+        <div className={`line ${isMenuOpen ? 'open' : ''}`}></div>
+        <div className={`line ${isMenuOpen ? 'open' : ''}`}></div>
+      </div>
+      <ul className={isMenuOpen ? 'menu-open full-screen' : ''}>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/projects">Projects</Link></li>
         <li><Link to="/achievements">Achievements</Link></li>
